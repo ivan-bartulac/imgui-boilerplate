c6ad1f9b76a814a315fcc5dc3950a81479b50fa1
Ivan Bartulac <ivan.bartulac@gmail.com>
2019-08-12 15:28:50 +0200

    - Added checks for submodules in build scripts.

--- BuildDebug.bat
+++ BuildDebug.bat
@@ -13,41 +13,43 @@ set binary_dir=bin
 set obj_dir_name=_intermediate
 set obj_dir=%binary_dir%\%obj_dir_name%
 
+call :ARE_MODULES_INITIALIZED
+
 if "%1" == "release" (
-    set target="release"
+	set target="release"
 )
 
 if "%1" == "clean" (
-    if exist %obj_dir% (
-        echo Cleaning Project: %project_name%
-        rmdir /Q /S %obj_dir%
-    )
-    call :CHECK_FAIL
-    goto :EOF
+	if exist %obj_dir% (
+		echo Cleaning Project: %project_name%
+		rmdir /Q /S %obj_dir%
+	)
+	call :CHECK_FAIL
+	goto :EOF
 )
 
 if %target% == "release" (
-    set target_options=/D_RELEASE /O2 /Ot
+	set target_options=/D_RELEASE /O2 /Ot
 ) else (
-    set project_name=%project_name%_%target%
-    set target_options=/D_DEBUG /Zi /Od 
+	set project_name=%project_name%_%target%
+	set target_options=/D_DEBUG /Zi /Od 
 )
 
 if not exist %obj_dir% (
-    call :CREATE_OBJ_DIR
+	call :CREATE_OBJ_DIR
 )
 
 if not defined DevEnvDir (
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+	call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 )
 
 echo Building Project: %project_name%
 pushd %obj_dir%
-    set cpp_files=%source_dir%\main.cpp %module_dir%\imgui\imgui.cpp %module_dir%\imgui\imgui_demo.cpp %module_dir%\imgui\imgui_draw.cpp %module_dir%\imgui\imgui_widgets.cpp %module_dir%\imgui\examples\imgui_impl_glfw.cpp %module_dir%\imgui\examples\imgui_impl_opengl3.cpp
-    cl /cgthreads8 /nologo %target_options% /I%include_dir% /I%module_dir% /I%module_dir%\imgui /Tp %cpp_files% /Tc %library_src_dir%\gl3w.c /link /SUBSYSTEM:WINDOWS /ENTRY:"mainCRTStartup" /OUT:..\%project_name%.exe opengl32.lib gdi32.lib User32.lib shell32.lib %library_dir%\glfw3.lib
-    if exist ..\%project_name%.ilk (
-        del ..\%project_name%.ilk
-    )
+	set cpp_files=%source_dir%\main.cpp %module_dir%\imgui\imgui.cpp %module_dir%\imgui\imgui_demo.cpp %module_dir%\imgui\imgui_draw.cpp %module_dir%\imgui\imgui_widgets.cpp %module_dir%\imgui\examples\imgui_impl_glfw.cpp %module_dir%\imgui\examples\imgui_impl_opengl3.cpp
+	cl /cgthreads8 /nologo %target_options% /I%include_dir% /I%module_dir% /I%module_dir%\imgui /Tp %cpp_files% /Tc %library_src_dir%\gl3w.c /link /SUBSYSTEM:WINDOWS /ENTRY:"mainCRTStartup" /OUT:..\%project_name%.exe opengl32.lib gdi32.lib User32.lib shell32.lib %library_dir%\glfw3.lib
+	if exist ..\%project_name%.ilk (
+		del ..\%project_name%.ilk
+	)
 popd
 call :CHECK_FAIL
 
@@ -56,17 +58,29 @@ goto :EOF
 
 :CHECK_FAIL
 if NOT ["%errorlevel%"]==["0"] (
-    pause
-    exit /b %errorlevel%
+	pause
+	exit /b %errorlevel%
 )
 exit /b 0
 
 :CREATE_OBJ_DIR
 pushd %binary_dir%
-    echo Creating Directory: %obj_dir_name%
-    mkdir %obj_dir_name%
-    call :CHECK_FAIL
+	echo Creating Directory: %obj_dir_name%
+	mkdir %obj_dir_name%
+	call :CHECK_FAIL
 popd
+exit /b 0
+
+:ARE_MODULES_INITIALIZED
+setlocal
+set _TMP=
+for /f "delims=" %%a in ('dir /b "etc/modules/imgui"') do set _TMP="%%a"
+IF {%_TMP%}=={} (
+	echo Submodules missing. Run command "git submodule update --init" to initialize and update Submodules.
+	pause
+	exit
+)
+endlocal
 exit /b 0
 
 :: ---- Function Section End
